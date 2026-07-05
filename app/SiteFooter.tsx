2a705505e684caf7e8370d0b8ad41f951218f0c8
0xchukss <0xchukss@users.noreply.github.com>
2026-07-05 03:43:07 +0200

    feat: restructure footer and add docs section

--- app/SiteFooter.tsx
+++ app/SiteFooter.tsx
@@ -7,10 +7,9 @@ export function SiteFooter() {
           <p>Autonomous nanopayments for Nigerian creator communities, priced locally and settled on Arc in USDC.</p>
         </div>
         <div>
-          <span>Product</span>
-          <a href='/use-link'>Use KoboLink</a>
-          <a href='/workflows?mode=fan'>Fan workflow</a>
-          <a href='/#how-it-works'>How it works</a>
+          <span>Legal</span>
+          <a href='/privacy'>Privacy Policy</a>
+          <a href='/terms'>Terms of Service</a>
         </div>
         <div>
           <span>Rails</span>
@@ -19,9 +18,10 @@ export function SiteFooter() {
           <a href='/workflows?mode=fan#x402-settlement'>x402 payments</a>
         </div>
         <div>
-          <span>Legal</span>
-          <a href='/privacy'>Privacy Policy</a>
-          <a href='/terms'>Terms of Service</a>
+          <span>Docs</span>
+          <a href='https://github.com/0xchukss/kobolink' target='_blank' rel='noreferrer'>GitHub Repo</a>
+          <a href='https://docs.arc.network' target='_blank' rel='noreferrer'>Arc Docs</a>
+          <a href='https://github.com/0xchukss/kobolink#readme' target='_blank' rel='noreferrer'>KoboLink Docs</a>
         </div>
       </div>
     </footer>
